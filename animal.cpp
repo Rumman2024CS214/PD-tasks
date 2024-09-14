6aa8ab3c8044b7de68b9632d7bd22dfe7f76e0d8
Rumman2024CS214 <rummanahmedra237@gmail.com>
2024-09-14 08:49:55 +0500

    message

---
+++ animal.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+       cout<<"  --------------------------------------------------"<<endl;
+       cout<<"                                                    "<<endl;  
+       cout<<"              o    ^___^                            "<<endl;
+       cout<<"                                                    "<<endl; 
+       cout<<"               o   (o o)\\__________                "<<endl;
+       cout<<"                                    \\              "<<endl; 
+       cout<<"                   (___)\\           )\\/\\        "<<endl;
+       cout<<"                         ||------w |                "<<endl;
+       cout<<"                         ||       ||                "<<endl;
+}
---
+++ gun.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"           +---^----------,--------,-----,--------^-,   "<<endl;
+    cout<<"            |  |||||||||   '--------'     |          0  "<<endl; 
+    cout<<"            '+--------------------------------------|   "<<endl;
+    cout<<"              ',---------,---------,--------------,     "<<endl; 
+    cout<<"               //XXXXXX//'|       //'                   "<<endl; 
+    cout<<"              //XXXXXX//  |      //'                    "<<endl;
+    cout<<"             //XXXXXX//'--------'                       "<<endl;
+    cout<<"            //XXXXXX//                                  "<<endl;
+    cout<<"           //XXXXXX//                                   "<<endl;
+    cout<<"          (---------)                                   "<<endl;
+    cout<<"           '-------'                                    "<<endl;
+} 
+                                 
